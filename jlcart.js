0c64e5f57996748767fe9cec472b1a7136abb865
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2023-11-17 13:06:42 +0100

    ui

--- jlcart.js
+++ jlcart.js
@@ -388,7 +388,7 @@ const loadData = async () => {
     if (localStorage.getItem('data') == null) {
         productRaw = await (await getProductsFromStripe()).json()
         productsJSON = productRaw
-        localStorage.setItem(JSON.stringify(productsJSON))
+        localStorage.setItem('data', JSON.stringify(productsJSON))
     }
     else 
         productsJSON = JSON.parse(localStorage.getItem('data'))
