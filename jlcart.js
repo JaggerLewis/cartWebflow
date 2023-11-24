6febc28cf734fe17505e441aecf6e4fa5c012d13
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2023-11-24 10:50:25 +0100

    show/hide total

--- jlcart.js
+++ jlcart.js
@@ -452,9 +452,9 @@ const initAbonnement = async () => {
             document.querySelector('#abo-prix-family-premium').textContent = datas[2].prices[0].price + '€/ an'
             document.querySelector('#abo-prix-starter-family').textContent = datas[1].prices[0].price + '€/ an'
             document.querySelector('#abo-prix-starter').textContent = datas[0].prices[1].price + '€/ an'
-            // document.querySelector('#total-family-premium').parentElement.style.display = 'none'
-            // document.querySelector('#total-starter-family').parentElement.style.display = 'none'
-            // document.querySelector('#total-starter').parentElement.style.display = 'none'
+            document.querySelector('#total-family-premium').parentElement.style.display = 'none'
+            document.querySelector('#total-starter-family').parentElement.style.display = 'none'
+            document.querySelector('#total-starter').parentElement.style.display = 'none'
             updateTime(false)
 
         }
@@ -464,9 +464,9 @@ const initAbonnement = async () => {
             document.querySelector('#abo-prix-family-premium').textContent = datas[2].prices[1].price + '€/ mois'
             document.querySelector('#abo-prix-starter-family').textContent = datas[1].prices[1].price + '€/ mois'
             document.querySelector('#abo-prix-starter').textContent = datas[0].prices[0].price + '€/ mois'
-            // document.querySelector('#total-family-premium').parentElement.style.display = 'block'
-            // document.querySelector('#total-starter-family').parentElement.style.display = 'block'
-            // document.querySelector('#total-starter').parentElement.style.display = 'block'
+            document.querySelector('#total-family-premium').parentElement.style.display = 'block'
+            document.querySelector('#total-starter-family').parentElement.style.display = 'block'
+            document.querySelector('#total-starter').parentElement.style.display = 'block'
             updateTime(true)
         }
     })
