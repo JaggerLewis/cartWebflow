3f3919ed50216c3492fcab69c978c562dded1b12
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2023-11-27 14:17:12 +0100

    ts-abonnement

--- jlcart.js
+++ jlcart.js
@@ -467,7 +467,7 @@ const loadAbonnement = async () => {
         answer = JSON.parse(localStorage.getItem('abonnement'))
     if (date - JSON.parse(localStorage.getItem('ts-abonnement')) > 600000) {
         answer = await (await getAbonnementFromStripe()).json()
-        localStorage.setItem('ts-abonnemnt', date)
+        localStorage.setItem('ts-abonnement', date)
         localStorage.setItem('abonnement', JSON.stringify(answer))
     }
     loaderContainer.style.display = 'none'
