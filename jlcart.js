b5a71f267a0483a182d7d820200764d38826b276
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2023-12-05 11:17:50 +0100

    update p baside

--- jlcart.js
+++ jlcart.js
@@ -20,7 +20,7 @@ modalDiv.classList.add("modal")
 modalDiv.setAttribute("class", "modal fade")
 snack.id = 'jl-snackbar'
 document.querySelector("body").appendChild(snack)
-modalDiv.innerHTML = '<div class="modal-dialog modal-lg" role="document"><div class="jl-modal"><div class="jl-header"><p class="title">Panier</p><img class="close-button hover" data-dismiss="modal" src="https://webcart.jagger-lewis.com/asset/icon_close.png"></img></div><div class="jl-border-container"><div id="jl-no-display" class="jl-no-display">Aucun élément sélectionné</div></div><div class="jl-bottom-container"><p class="jl-bottom-text">Frais de livraison (Standard)</p><p class="jl-bottom-text">5.99€</p></div><div class="jl-container-total"><p class="jl-total-title">Total</p><p class="jl-total-title" id="jl-total">0&euro;</p></div><button id="validate-cart" class="jl-button">Finaliser la commande</button></div></div>'
+modalDiv.innerHTML = '<div class="modal-dialog modal-lg" role="document"><div class="jl-modal"><div class="jl-header"><p class="title jl-p">Panier</p><img class="close-button hover" data-dismiss="modal" src="https://webcart.jagger-lewis.com/asset/icon_close.png"></img></div><div class="jl-border-container"><div id="jl-no-display" class="jl-no-display">Aucun élément sélectionné</div></div><div class="jl-bottom-container"><p class="jl-p jl-bottom-text">Frais de livraison (Standard)</p><p class=" jl-p jl-bottom-text">5.99€</p></div><div class="jl-container-total"><p class=" jl-p jl-total-title">Total</p><p class="jl-p jl-total-title" id="jl-total">0&euro;</p></div><button id="validate-cart" class="jl-button">Finaliser la commande</button></div></div>'
 body.appendChild(modalDiv)
 body.insertBefore(loaderContainer, document.body.firstChild);
 
@@ -737,13 +737,13 @@ const showCart = (event) => {
         let RowElem3 = document.createElement('div')
         let image = document.createElement('img')
         RowElem1.classList.add('jl-product-row')
-        RowElem1.innerHTML = "<p class='jl-product-name'>"+ prod.id.name+"</p><p class='jl-product-price'>" + displayPrice(prod.id.price.price) + "€</p>"
+        RowElem1.innerHTML = "<p class='jl-p jl-product-name'>"+ prod.id.name+"</p><p class='jl-p jl-product-price'>" + displayPrice(prod.id.price.price) + "€</p>"
         RowElem2.classList.add('jl-product-row')
-        RowElem2.innerHTML = "<p class='jl-product-quantity'>Qté : "+ prod.quantity + " </p>"
+        RowElem2.innerHTML = "<p class='jl-p jl-product-quantity'>Qté : "+ prod.quantity + " </p>"
        
         
         RowElem3.classList.add('jl-product-row-end')
-        RowElem3.innerHTML = "<img class='close-button'  src='https://webcart.jagger-lewis.com/asset/icon_info.png'><p class='jl-text-hint'>Choisissez un abonnement après votre achat. </p> <img class='close-button hover button-small' id='remove-"  + id + "' src='https://webcart.jagger-lewis.com/asset/icon_trash.png'></img >"
+        RowElem3.innerHTML = "<img class='close-button'  src='https://webcart.jagger-lewis.com/asset/icon_info.png'><p class='jl-p jl-text-hint'>Choisissez un abonnement après votre achat. </p> <img class='close-button hover button-small' id='remove-"  + id + "' src='https://webcart.jagger-lewis.com/asset/icon_trash.png'></img >"
         image.src = prod.id.image
         image.classList.add('jl-product-img')
         containerProduct.classList.add('jl-container-product')
