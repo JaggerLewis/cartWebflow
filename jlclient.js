f8b7e452954550258f0883b0fbcbab659ee34051
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-04-15 16:06:54 +0200

    WIP: remove import

--- jlclient.js
+++ jlclient.js
@@ -1,7 +1,6 @@
 
 
 const baseurl = 'https://app-api.mypet.fit'
-const interfaceUrl = "https://heyjag.mypet.fit";
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY1NzFkM2RjNzA4M2E3ODg2ZTQzNzNhOCIsInBob25lIjoiMDAzMzYxMjk2NTM5OCIsIm5hbWUiOiJFbGlvdCIsImxhc3RuYW1lIjoiTUFSVElOIiwiZW1haWwiOiJlbGlvdC5tYXJ0aW5AamFnZ2VyLWxld2lzLmNvbSJ9LCJpYXQiOjE3MTIxMzM0Njh9.cfvU9bp8yr8ASiMN5vY9j5mrQH8CfV50m1k3Hny917Y'
 
 const step2 = 'activation-produit-etape02'
