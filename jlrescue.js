73c2e3396f0ae73a0da0b2e8424e779c081a9f80
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-07 15:21:12 +0200

    start init rescue

---
+++ jlrescue.js
@@ -0,0 +1,7 @@
+const initRescue = (btn) => {
+    console.log('rescue', btn)
+}
+
+if (document.getElementById('jl-rescue-action')) {
+    initRescue(document.getElementById('jl-rescue-action'))
+}
