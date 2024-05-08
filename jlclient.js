08c1ac8233d4e8a6658b2183c0d5fcdcbb5df5d2
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-08 11:10:10 +0200

    bla

--- jlclient.js
+++ jlclient.js
@@ -97,7 +97,7 @@ const initGeoFencingSwitch = (node) => {
     let data = JSON.parse(dog.collar.settings.geofencing)
     node.innerHTML = `<label class="switch"><input type="checkbox"><span class="slider round" id="jag-geofencing-slider" selected="${data.active == true}"></span></label>`
     let select = document.getElementById('jag-geofencing-slider')
-    node.addEventListener('click', () => select.setAttribute('selected', select.getAttribute('selected') == 'true') )
+    node.addEventListener('click', () => select.setAttribute('selected', !(select.getAttribute('selected') == 'true')) )
 }
 const deleteAccountEmail = async () => {
     let res = await fetch(baseurl + '/user/delete/email', {
