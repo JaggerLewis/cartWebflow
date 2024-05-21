7f3e3f33ce4e0edd825516506d8eb705e740a2b1
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-21 12:36:46 +0200

    bla

--- jlclient.js
+++ jlclient.js
@@ -1006,19 +1006,16 @@ const validateAction = async () => {
             "serialNumber": serial,
             "phoneToken": value
         }), 
-      }) 
+      }).then(async (res) => res?.json())
       
-      if (result.status == 200) {
-        let json = await result.json()
-        console.log(json)
-        if (json.activated) {
+      if (result) {
+        if (result.activated) {
             window.open(REDIRECT.active_3 + '?session_id=725', '_self')
         }
         else {
             return
             window.open(REDIRECT.active_2, '_self')
         }
-        console.log(json);
       }
       else {
         showAddCart('Code incorrect', true)
