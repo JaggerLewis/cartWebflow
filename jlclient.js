b7bff475210758ea22e3cc736ba2b6acc57032e2
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-22 15:37:23 +0200

    subFormula

--- jlclient.js
+++ jlclient.js
@@ -575,7 +575,7 @@ const initOption = async () => {
                 document.getElementById('jag-abo-actif' ).style.display = 'none'
                 document.getElementById('jag-abo-stop' ).style.display = 'none'
                 document.getElementById('jag-abo-resilli-text').innerHTML = dog.collar.formula_subscription.timeout - Date.now() < 0 ? "Votre abonnement est terminé depuis le<br>"+ getDate(dog.collar.formula_subscription.timeout) :  'Votre abonnement est résilié.<br>Il se termine le ' + getDate(dog.collar.formula_subscription.timeout)
-                document.getElementById('jag-'+type+'-name').innerHTML = subFormula.name
+                document.getElementById('jag-'+type+'-name').innerHTML = subFormula?.name ?? 'Formule à vie'
                 // document.getElementById('jag-abo-stoped-action' )?.style.display = dog.collar.formula_subscription.timeout - Date.now() < 0 ? 'none' : 'flex'
                 // document.getElementById('jag-abo-stoped-action' )?.addEventListener('click', () => restartAbo())
                 check = document.getElementById('jag-abo-check')
@@ -589,7 +589,7 @@ const initOption = async () => {
                 document.getElementById('jag-abo-actif' ).style.display = 'flex'
                 document.getElementById('jag-abo-resilli' ).style.display = 'none'
                 document.getElementById('jag-abo-stop' ).style.display = 'flex'
-                document.getElementById('jag-'+type+'-name').innerHTML = subFormula.name
+                document.getElementById('jag-'+type+'-name').innerHTML = subFormula?.name ?? 'Formule à vie'
                 // document.getElementById('jag-abo-stoped-action' )?.style.display = 'none'
                 document.getElementById('jag-'+type+'-actif-start').innerHTML = 'Abonnement actif depuis le '+ getDate(dog.collar.formula_subscription.subscription_date)
                 document.getElementById('jag-'+type+'-actif-end').innerHTML = "Jusqu'au " + getDate(dog.collar.formula_subscription.timeout)
