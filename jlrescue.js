5a47930eb5a338b27b736ce1d1fa2c7fe093b8eb
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-22 16:17:27 +0200

    set rescue marker

--- jlrescue.js
+++ jlrescue.js
@@ -76,7 +76,7 @@ const tracks = async (key) => {
                     map: map,
                     position: pos,
                     title: "",
-                    icon : `https://assets-global.website-files.com/6549f4ba8294cf140608d893/6617bddf381409947dbd68e7_Capture%20d%E2%80%99e%CC%81cran%202024-04-11%20a%CC%80%2012.36.54.png`
+                    icon : `https://assets-global.website-files.com/6549f4ba8294cf140608d893/664c893a5d2b02d82784dbdc_imagedog.png`
                 });
                 markers.push(tmp)
                 break
