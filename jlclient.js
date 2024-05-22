3f0deec6f006b44d9e7394f0c9de25a5adfb39f2
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-22 15:06:00 +0200

    ui

--- jlclient.js
+++ jlclient.js
@@ -559,8 +559,8 @@ const initOption = async () => {
             method: "POST",
             headers : header,
           })
-          await getDog(dog._id)
-          getAll() 
+        await getDog(dog._id)
+        getAll() 
     }
 
     let types = ['abo']
@@ -575,15 +575,15 @@ const initOption = async () => {
                 document.getElementById('jag-abo-actif' ).style.display = 'none'
                 document.getElementById('jag-abo-stop' ).style.display = 'none'
                 document.getElementById('jag-abo-resilli-text').innerHTML = dog.collar.formula_subscription.timeout - Date.now() < 0 ? "Votre abonnement est terminé depuis le<br>"+ getDate(dog.collar.formula_subscription.timeout) :  'Votre abonnement est résilié.<br>Il se termine le ' + getDate(dog.collar.formula_subscription.timeout)
-                document.getElementById('jag-abo-stoped-action' ).style.display = dog.collar.formula_subscription.timeout - Date.now() < 0 ? 'none' : 'flex'
-                document.getElementById('jag-abo-stoped-action' ).addEventListener('click', () => restartAbo())
+                document.getElementById('jag-abo-stoped-action' )?.style.display = dog.collar.formula_subscription.timeout - Date.now() < 0 ? 'none' : 'flex'
+                document.getElementById('jag-abo-stoped-action' )?.addEventListener('click', () => restartAbo())
                 break;
             case 'active':
                 document.getElementById('jag-abo-stoped' ).style.display = 'none'
                 document.getElementById('jag-abo-actif' ).style.display = 'flex'
                 document.getElementById('jag-abo-resilli' ).style.display = 'none'
                 document.getElementById('jag-abo-stop' ).style.display = 'flex'
-                document.getElementById('jag-abo-stoped-action' ).style.display = 'none'
+                document.getElementById('jag-abo-stoped-action' )?.style.display = 'none'
                 document.getElementById('jag-'+type+'-actif-start').innerHTML = 'Abonnement actif depuis le '+ getDate(dog.collar.formula_subscription.subscription_date)
                 document.getElementById('jag-'+type+'-actif-end').innerHTML = "Jusqu'au " + getDate(dog.collar.formula_subscription.timeout)
                 break;
