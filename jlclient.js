c0aa4c087695244bfd0efcf36e0bb794e82b3094
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-28 10:03:37 +0200

    set price

--- jlclient.js
+++ jlclient.js
@@ -150,14 +150,14 @@ const showAboHistoric = async () => {
     .then(async (res) => await res.json())
     .then((res) => res.orders)
 
-    orders.filter((elem) => elem.type != 'product').forEach((order) => {
+    orders.filter((elem) => elem.type != 'product').reverse().forEach((order) => {
         let newCard = card.cloneNode(true)
         changeChildsId(newCard, '-'+order._id, 'jag-')
         card.parentElement.appendChild(newCard)
         let date  = new Date(order.createdAt)
 
         document.getElementById('jag-abo-historic-date-' + order._id).innerHTML =  date.getDate() + '/' + (date.getMonth() + 1) + '/' +  date.getFullYear();
-        document.getElementById('jag-abo-historic-type-' + order._id).innerHTML = getType(order.type)
+        document.getElementById('jag-abo-historic-type-' + order._id).innerHTML = order.total.total ?? 100 / 100  + '€'
         document.getElementById('jag-abo-historic-pdf-'+order._id).addEventListener('click', async () => {
             loaderContainer.style.display = 'flex'
             fetch(baseurl + '/order/'+ order._id +'/pdf', {
