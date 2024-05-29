27d07b959a697fbaac493ab61043b24f971312ca
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-05-29 11:41:19 +0200

    false

--- jlcart.js
+++ jlcart.js
@@ -933,13 +933,13 @@ const initAboB = async () => {
     document.getElementById('jl-abo-year-price').innerHTML = (formula.find((elem) => elem.metadata.pricing == 'yearly').prices[0].price / 12).toFixed(2) + '€' + getTrad('/mois', '/month')
     document.getElementById('jl-abo-life-price').innerHTML = formula.find((elem) => elem.metadata.pricing == 'life').prices[0].price.toFixed(2) + '€'
 
-    let active = formula.find((elem) => elem.metadata.pricing == 'monthly').prices[0].activation == 'true'
+    let active = formula.find((elem) => elem.metadata.pricing == 'monthly').prices[0].metadata.activation == 'false'
     document.getElementById('jl-abo-month-price-info').innerHTML = getTrad('Sans engagement', 'Without obligation') + (active ? getTrad("<br>Sans frait d'activation", '<br>Withour activation fee') : '')
     //document.getElementById('jl-abo-year-price-info').innerHTML = getTrad('Soit 3 mois gratuits', '3 months free')
     let annualPrice = formula.find((elem) => elem.metadata.pricing == 'yearly').prices[0].price.toFixed(2) + '€'
-    active = formula.find((elem) => elem.metadata.pricing == 'yearly').prices[0].activation == 'false'
+    active = formula.find((elem) => elem.metadata.pricing == 'yearly').prices[0].metadata.activation == 'false'
     document.getElementById('jl-abo-year-price-info').innerHTML = getTrad('Soit ' + annualPrice + ' par an', 'or ' + annualPrice + ' per year')+ (active ? getTrad("<br>Sans frait d'activation", '<br>Withour activation fee') : '')
-    active = formula.find((elem) => elem.metadata.pricing == 'life').prices[0].activation == 'false'
+    active = formula.find((elem) => elem.metadata.pricing == 'life').prices[0].metadata.activation == 'false'
     let lifePrice36 = (formula.find((elem) => elem.metadata.pricing == 'life').prices[0].price / 36).toFixed(2) + '€'
     document.getElementById('jl-abo-life-price-info').innerHTML = getTrad('Soit ' + lifePrice36 + '/mois sur 3 ans', 'Or ' + lifePrice36 + '/month over 3 years')+ (active ? getTrad("<br>Sans frait d'activation", '<br>Withour activation fee') : '')
 }
