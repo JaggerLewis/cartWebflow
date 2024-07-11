62d980ef4cfee683ab1d7b5348430d689b169868
EliotMartinJag <135212004+EliotMartinJag@users.noreply.github.com>
2024-07-11 14:53:55 +0200

    bla

--- jlclient.js
+++ jlclient.js
@@ -1673,7 +1673,7 @@ const disableButton = () => {
     for (elem of document.querySelectorAll('[id=jl-product-abo-container]')) {
         elem.onclick = () => showAddCart('Vous ne pouvez pas séléctionner cette formule...', false);
         elem.classList.add('selected')
-        if (elem.getAttribute('jl_productid') == currentAbo)
+        if (elem.getAttribute('jl_productid') == abo)
         return
     }
 }
