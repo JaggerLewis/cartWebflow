51234473de1d5d472041e83d7761de8435fab7e0
Maxime-JaggerLewis <135699467+Maxime-JaggerLewis@users.noreply.github.com>
2025-06-20 11:22:01 +0200

    reverse addItem

--- jlcart.js
+++ jlcart.js
@@ -427,9 +427,9 @@ class ShoppingCart {
         this.cart.length = 0;
         const cart_items = [];
         for (const product of cart) {
-            this.addItem(product.id, product.quantity);
-            //const cardProduct = new ProductCart(product.id, product.quantity)
-            //this.cart.push(cardProduct)
+            //this.addItem(product.id, product.quantity);
+            const cardProduct = new ProductCart(product.id, product.quantity)
+            this.cart.push(cardProduct)
             cart_items.push({
                 'item_id': product.id.metadata.productId,
                 'item_name': product.id.metadata.title_fr,
